b8aed7ce67a2090f69dc2f36715458e3fbaaa73e
ark314 <arklp20909@gmai.com>
2020-11-29 14:05:06 +0900

    make it possible to start kbdmouse

--- activate.sh
+++ activate.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+DIR_SCRIPT=$(cd $(dirname $0);pwd)
+echo $DIR_SCRIPT
 if [ "$(ps -a|grep "kbdmouse")" = "" ]; then
 	zenity --question --text="キーボードマウスを有効化しますか？\n（テンキーが必要です）" --ok-label="はい" --cancel-label="いいえ" --width 250
 	if [ $? != 0 ]; then
 		exit 1
 	else
 		#有効化処理
+		pkexec $DIR_SCRIPT/kbdmouse $($DIR_SCRIPT/search_device.sh)
 		:
 	fi
 else
